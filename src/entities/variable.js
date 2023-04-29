30c03ca4123bfce62dae8ecec4c604fa3ba27f27
Mike <odjomike25@gmail.com>
2023-04-29 23:05:05 +0100

    visual  representation of a variable

--- src/entities/variable.js
+++ src/entities/variable.js
@@ -1,6 +1,26 @@
-import { abstractComponent } from "../../abstraction/abstractComponent";
+//import { abstractComponent } from "../../abstraction/abstractComponent";
+var actions = [
+    {name: "deletion", path: "src/images/"},
+    {name: "path", path: "src/images/"},
+    {name: "decorator", path: "src/images/"},
+    {name: "adapter", path: "src/images/"},
+    {name: "composite", path: "src/images/"},
+    {name: "strategy", path: "src/images/"},
+    {name: "chain of responsibility", path: "src/images/"}
+];
 
-class Variable extends abstractComponent{
-    constructor(){
+class Variable {
+    constructor(props = {name: "dummy", style: "template"}){
+        
+        if(["template" ,"query" ,"plain"].indexOf(props.style)==-1)
+            throw new Error("the style must be a correct style");
+        this.name = props.name;
+        this.style = props.style;
+
+        this._shape = aya.Component("lozenge", {x:0, y:0, width:10, height:15});
+        this._shape.shape.setStyles({strokewidth: "2px", stroke: "black", fill: "yellow"});
+        var text = aya.Text(aya.id(), this._shape.shape.x, this._shape.shape.y, props.name, 0, this._shape.shape.x + this._shape.shape.width, this._shape.shape.y);
+        this._shape.addChild(text, {x: 0, y: -10});
     }
+
 }
--- tests/path.test.js
+++ tests/path.test.js
@@ -1,5 +1,45 @@
 QUnit.module('Path');
 
-QUnit.test("", assert =>{
-    
+/***************************** visual representation of a path *********************************/
+
+QUnit.test("default visual representation of a path", assert =>{
+	var p = new Path("/");
+
+	assert.equal(p.shape.type, "circle", "the shape must be a circle");
+	assert.equal(p.shape.form.r, 15, "the circle radius must be 15px");
+	assert.equal(p.shape.form["stroke-width"], "2px", "the border width must be 2px");
+	assert.equal(p.shape.form["stroke"], "green", "the border color must be green");
+	assert.equal(p.shape.form["fill"], "gray", "the circle color must be gray");
+});
+
+QUnit.test("checking that the shape is effectively a circle", assert =>{
+	
+});
+
+
+QUnit.test("throws an exception when the path attribute isn't defined", assert =>{
+
+    assert.throws(function(){
+    	new Path();
+    },"path's creation requires a name");
+});
+
+QUnit.test("throws an exception when the first character of a path isn't a /", assert =>{
+	
+});
+
+QUnit.test("throws an exception when it appears two consecutive / symbols", assert =>{
+	
+});
+
+QUnit.test("throws an exception when the first character of a path isn't a /", assert =>{
+	
+});
+
+QUnit.test("throws an exception when the first character of a path isn't a /", assert =>{
+	
+});
+
+QUnit.test("throws an exception when uri portion doesn't respect REST specification", assert =>{
+	
 });
